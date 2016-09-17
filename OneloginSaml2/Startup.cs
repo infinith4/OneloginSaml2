a261faf340803ccff5d4ade511912a3ddd2a42fa
tashiro.hiroshi <tashiro.hiroshi>
2016-09-17 20:31:40 +0900

    first commit

---
+++ OneloginSaml2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneloginSaml2.Startup))]
+namespace OneloginSaml2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
